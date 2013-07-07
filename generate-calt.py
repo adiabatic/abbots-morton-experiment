ce189a1cbff98d9afc08c03cd08c9a0839f190f9
Nathan Galt <adiabatic@users.noreply.github.com>
2013-07-06 23:43:55 -0700

    get .Utter and .Roe working together

--- generate-calt.py
+++ generate-calt.py
@@ -7,7 +7,7 @@ class Context(object): pass
 class Predicates(object):
     
     @classmethod
-    def _generic(self, pred, glyph):
+    def _any_cxn(self, pred, glyph):
         r = False
         try:
             cxns = itertools.chain.from_iterable(glyph['connection sets'])
@@ -22,23 +22,37 @@ class Predicates(object):
     def can_s2(self, glyph):
         def f(cxn):
             return cxn.get('side') == 'left' and cxn.get('height') == 'short'
-        return self._generic(f, glyph)
+        return self._any_cxn(f, glyph)
         
     @classmethod
     def can_2s(self, glyph):
         def f(cxn): return cxn.get('side') == 'right' and cxn.get('height') == 'short'
-        return self._generic(f, glyph)
+        return self._any_cxn(f, glyph)
+            
+    @classmethod
+    def can_s2s(self, glyph):
+        have_lhs = False
+        have_rhs = False
+        
+        for cset in glyph['connection sets']:
+            for cxn in cset:
+                try:
+                    if cxn['side'] == 'left'  and cxn['height'] == 'short': have_lhs = True
+                    if cxn['side'] == 'right' and cxn['height'] == 'short': have_rhs = True
+                except: pass
+        return have_lhs and have_rhs
+            
             
     @classmethod
     def can_b2(self, glyph):
         def f(cxn):
             return cxn.get('side') == 'left' and cxn.get('height') == 'baseline'
-        return self._generic(f, glyph)
+        return self._any_cxn(f, glyph)
         
     @classmethod
     def can_2b(self, glyph):
         def f(cxn): return cxn.get('side') == 'right' and cxn.get('height') == 'baseline'
-        return self._generic(f, glyph)
+        return self._any_cxn(f, glyph)
             
 
     
@@ -54,6 +68,12 @@ lookup plain_to_s2 {{
     {0.plain_to_s2}
 }} plain_to_s2; 
 
+lookup s2_to_s2s {{
+    {0.s2_to_s2s}
+}} s2_to_s2s;
+
+# not sure if I need 2s_to_s2s
+
 feature calt {{
 
     {0.can_2s}
@@ -61,6 +81,9 @@ feature calt {{
     
     {0.does_2s}
     {0.does_s2}
+    
+    {0.can_s2s}
+    {0.does_s2s}
     
     ###############################
     ## Pass 1: Connecting the Unconnected
@@ -75,14 +98,21 @@ feature calt {{
     # at the Short height
     sub @can_2s @can_s2' lookup plain_to_s2;
     sub @can_2s' lookup plain_to_2s @can_s2;
+
     
     ###############################
-    ## Pass 2: Upgrading the Half-Connected
+    ## Pass 2: Reaching Out to Those Who Care
     ##
     
     sub @does_2s @can_s2' lookup plain_to_s2;
     sub @can_2s' lookup plain_to_2s @does_s2;
-    {0.calt_pass_2}
+
+
+    ###############################
+    ## Pass 3: Made One Connection, Make Another
+    ##
+    
+    sub @does_s2' lookup s2_to_s2s @does_s2; 
 }} calt;
 """
 
@@ -94,19 +124,6 @@ glyphs = list(yaml.safe_load(y))
 def classnameize(names):
     return ' '.join(itertools.chain(["["], names, ["]"]))
 
-
-
-
-def new_can2b(glyph):
-    r = False
-    try:
-        cxns = itertools.chain.from_iterable(glyph['connection sets'])
-        for cxn in cxns:
-            if cxn.get('side') == 'right' and cxn.get('height') == 'baseline':
-                r = True
-                break
-    except: pass
-    return r
 
 ####
 ## Baseline Height
@@ -129,11 +146,17 @@ CTX.calt_pass_1_baseline_2 = \
 
 
 
-# can/does start/end at short height horizontally
+# can/does start/end at short height horizontally on one side only
 can_s2  = [glyph['name'] for glyph in glyphs if pred.can_s2(glyph)]
 does_s2 = [x + ".s2" for x in can_s2]
 can_2s  = [glyph['name'] for glyph in glyphs if pred.can_2s(glyph)]
 does_2s = [x + '.2s' for x in can_2s]
+
+# can/does start/end at short height horizontally on both sides
+can_s2s  = [glyph['name'] for glyph in glyphs if pred.can_s2s(glyph)]
+does_s2s = [x + ".s2s" for x in can_s2s]
+
+
 
 ### lookups
 CTX.plain_to_2s = \
@@ -142,14 +165,20 @@ CTX.plain_to_2s = \
 CTX.plain_to_s2 = \
     '\n    '.join("sub {} by {};".format(can, does) for can, does in zip(can_s2, does_s2))
 
+CTX.s2_to_s2s = \
+    '\n    '.join("sub {} by {};".format(can, does) for can, does in zip(can_s2s, does_s2s))
+
 ### classes
-CTX.can_2s = "@can_2s = {};".format(classnameize(can_2s))
-CTX.can_s2 = "@can_s2 = {};".format(classnameize(can_s2))
-CTX.does_2s = "@does_2s = {};".format(classnameize(does_2s))
-CTX.does_s2 = "@does_s2 = {};".format(classnameize(does_s2))
+CTX.can_2s =   "@can_2s = {};".format(classnameize(can_2s))
+CTX.can_s2 =   "@can_s2 = {};".format(classnameize(can_s2))
+
+CTX.does_2s =  "@does_2s = {};".format(classnameize(does_2s))
+CTX.does_s2 =  "@does_s2 = {};".format(classnameize(does_s2))
+
+CTX.can_s2s  = "@can_s2s = {};".format(classnameize(can_s2s))
+
+CTX.does_s2s = "@does_s2s = {};".format(classnameize(does_s2s))
 
 
-# don't bug me now
-CTX.calt_pass_2 = ''
 
 print TEMPLATE.format(CTX)
