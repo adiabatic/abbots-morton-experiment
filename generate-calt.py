910483a8ec542c57b4922f5ce3dc50a97c6b439a
Nathan Galt <adiabatic@users.noreply.github.com>
2013-07-07 03:05:40 -0700

    add et, sub bottom connectors

--- generate-calt.py
+++ generate-calt.py
@@ -165,11 +165,19 @@ lookup calt_pass_2 {{
     # Short height
     sub @does_2s @can_s2' lookup plain_to_s2;
     sub @can_2s' lookup plain_to_2s @does_s2;
+    
+    # at the baseline
+    sub @does_2b @can_b2' lookup plain_to_b2;
+    sub @can_2b' lookup plain_to_2b @does_b2;
+    
 }} calt_pass_2;
 
 lookup calt_pass_3 {{
     sub @does_s2' lookup s2_to_s2s @does_s2;
     sub @does_2s' lookup _2s_to_s2s @does_s2;
+
+    sub @does_b2' lookup b2_to_b2b @does_b2;
+    sub @does_2b' lookup _2b_to_b2b @does_b2;
 }} calt_pass_3;
 
 
